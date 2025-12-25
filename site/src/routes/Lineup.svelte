1d300b96408fcff5010e84226ad7048d902cb55e
ken-morel <engonken8@gmail.com>
2025-12-26 00:43:14 +0100

    adding page to site

---
+++ site/src/routes/Lineup.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { GamaInstance } from "https://github.com/ken-morel/gama/blob/dev/runners/web/gama.js?raw=true";
+
+  const instance = new GamaInstance();
+
+  instance.bind(document.getElementById("lineup"));
+  instance.bindKeyboard(document);
+
+  instance.setup("/lineup.wasm").then(() => {
+    instance.start();
+  });
+</script>
+
+<canvas id="lineup" style="width: 100%;height: 100%;"></canvas>
