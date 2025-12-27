625a2e6618182ff5229dc1694a5831738f949773
ken-morel <engonken8@gmail.com>
2025-12-27 03:41:00 +0100

    worked on a test sample

--- test/src/main.c
+++ test/src/main.c
@@ -1,53 +1,37 @@
-#include "gama/3d/image.h"
-#include "gama/3d/scene.h"
-#include "gama/3d/transform.h"
-#include "gama/gama.h"
-#include "gama/image.h"
 #include <gama.h>
 #include <gama/3d.h>
 
 int main() {
-  gm_init(500, 500, "skeleton gama application");
+  gm_init(500, 500, "gama cube application");
   gm_show_fps(1);
+  gm_fullscreen(1);
 
-  gm3ObjFile file;
+  gm3ObjFile file; // create an obj file
 
-  int code = gm3_load_obj(&file, "assets/obj/cube.obj");
-  if (code != 0) {
-    printf("object load exited with code %d\n", code);
-    return 100;
-  }
+  int code = gm3_load_obj(&file, "assets/obj/test.obj"); // load the obj file
 
-  if (file.n_objects != 1) {
-    printf("File had %zu objects instead of one\n", file.n_objects);
-    return 101;
-  }
-
-  gm3Mesh *mesh = &file.objects[0];
-
-  printf("mesh 0 has %zu faces and %zu vertices\n", mesh->n_faces,
-         mesh->n_vertices);
+  printf("obj file has %zu objects", file.n_objects);
 
   gm3Scene scene = gm3_scene();
-
-  gm3_scene_create(&scene, 2, 2);
+  scene.light.color = GM_ORANGE;
 
   gm3Transform transform = gm3_transform();
-  transform.position.z = 5;
+  transform.position.z = 2;
+  transform.position.y -= 1;
 
-  gm3Image img;
+  gm3Image img; // the image where we snap the 3d object into a 2d image and
+                // then draw
 
-  gm3_project(mesh, &transform, &scene, &img);
-
-  printf("outputs has %zu triangles\n", img.n_triangles);
-  printf("n colors: %zu\n", img.n_colors);
-
-  // create shapes
   do {
-    // do things here
-    transform.rotation.y += gm_dt();
-    gm3_project(mesh, &transform, &scene, &img);
-    gm3_draw_image(&img, -1, -1);
+    transform.rotation.y += gm_dt(); // update the rotation to make it rotate
+    // transform.rotation.z += gm_dt();
+    // transform.rotation.x += gm_dt();
+    for (size_t i = 0; i < file.n_objects;
+         i++) { // draw each of the objects in the file
+      gm3_project(&file.objects[i], &transform, &scene,
+                  &img);            // snap on the image
+      gm3_draw_image(&img, -1, -1); // draw the image
+    }
   } while (gm_yield());
   // destroy shapes
 
