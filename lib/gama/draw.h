8dac9f4f8616193da9f9eb536c242afad3a058b8
ken-morel <engonken8@gmail.com>
2026-01-11 16:13:11 +0100

    gemini implemented serialization and loader for gltf

--- lib/gama/draw.h
+++ lib/gama/draw.h
@@ -19,7 +19,11 @@
 // ------------------------- Immediate-Mode Primitives -----------------------
 // ---------------------------------------------------------------------------
 
-int gm_cache(unsigned int id) { return 1; }
+int gm_cache(unsigned int id) {
+  static int on = 0;
+  on = !on;
+  return on;
+}
 
 /**
  * @brief Draws a line segment.
