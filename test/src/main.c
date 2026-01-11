15957ac8436b83f8f0e3a29a2005e52d36d56cc8
ken-morel <engonken8@gmail.com>
2026-01-11 20:22:43 +0100

    working base gltf loader

--- test/src/main.c
+++ test/src/main.c
@@ -1,10 +1,11 @@
-#include "gama/3d/transform.h"
 #include <assets/gltf/cube.glb.h>
 #include <gama.h>
 #include <gama/3d.h>
+#include <gama/3d/gltf.h>
 #include <gama/debug.h>
 
 int main() {
+
   gm_init(0, 0, "Hello world");
   gm_show_fps(1);
 
@@ -12,24 +13,19 @@ int main() {
 
   gm3Mesh mesh = cube_mesh();
 
-  gmdn(pos3, mesh.faces[0].normal);
-  printf("%zu,", mesh.faces[0].vertices[0]);
-  printf("%zu,", mesh.faces[0].vertices[1]);
-  printf("%zu\n", mesh.faces[0].vertices[2]);
-
-  gmdn(pos3, mesh.vertices[0]);
-  gmdn(pos3, mesh.vertices[1]);
-  gmdn(pos3, mesh.vertices[2]);
-
   gm3Transform trans = gm3_transform();
-  trans.position.z = 3;
+  trans.position.z = 5;
 
   gm_background(0x333355FF);
 
   gm3Image img = gm3_image();
 
   do {
-    gm3_project(&img, &mesh, NULL, NULL);
+    if (gm_mouse.down) {
+      trans.rotation.x += gm_mouse.movement.y;
+      trans.rotation.y -= gm_mouse.movement.x;
+    }
+    gm3_project(&img, &mesh, &trans, NULL);
 
     gm3_draw_image(&img, 0, 0, 1);
 
